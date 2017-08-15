14ca3c55da7549e2a141455507b80639d73ac7ba
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:12 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostFunctionalConan(ConanFile):
     name = "Boost.Functional"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/functional"
+    url = "https://github.com/bincrafters/conan-boost-functional"
+    source_url = "https://github.com/boostorg/functional"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     requires =  "Boost.Level5Group/1.64.0@bincrafters/testing"
