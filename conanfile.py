f9e0af9a225692e5d46c5d087a2fb5bac00e257d
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:08 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostFunctionalConan(ConanFile):
     name = "boost_functional"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_functional"
-    
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["functional"]
     is_header_only = True
 
